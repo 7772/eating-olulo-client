a2bae1f996456409c73ca4c30e6cc27c83e9e3b6
landon <landon@olulo.io>
2020-01-19 18:55:55 +0900

    Get stores, meal-tikets list

--- src/components/MealTiketList/index.tsx
+++ src/components/MealTiketList/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import MealTiket from '../MealTiket';
+import {Alert} from 'reactstrap';
 
 
 const MealTiketList = ({mealTikets = []}: any) => {
-  return (
+  return mealTikets.length > 0 ? (
     mealTikets.map((mealTiket: any, index: any) => {
       return (
         <MealTiket key={index} mealTiket={mealTiket}/>
       );
     })
+  ) : (
+    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '30vh'}}>
+      <Alert className={'container'} color={'warning'}>
+        오늘은 아무도 안시켜먹나봐요..
+      </Alert>
+    </div>
   );
 };
 
--- src/screens/MainScreen/index.tsx
+++ src/screens/MainScreen/index.tsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
 
 import {NavBar, MealTiketList} from '../../components';
+import {MealTiketsAPI, StoresAPI} from '../../apis';
 
 
 class MainScreen extends Component<any, any> {
@@ -9,13 +10,47 @@ class MainScreen extends Component<any, any> {
     super(props);
 
     this.state = {
+      stores: [],
       mealTikets: [],
     };
   }
 
-  render() {
-    const {mealTikets} = this.state;
+  componentDidMount() {
+    this.getStoreList();
+    this.getMealTiketList();
+  }
 
+  async getStoreList() {
+    try {
+      const {response, json}: any = await StoresAPI.getList();
+
+      if (response.ok) {
+        this.setState({stores: json.stores});
+      } else {
+        // TODO: 401, 422 or some error
+      }
+    } catch (error) {
+      alert(error.message);  
+    }
+  }
+
+  async getMealTiketList() {
+    try {
+      const result: any = await MealTiketsAPI.getList();
+      
+      if (result.response.ok) {
+        this.setState({mealTikets: result.json.mealTikets});
+      } else {
+        // TODO: 401, 422 or some error
+      }
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  render() {
+    const {stores, mealTikets} = this.state;
+    
     return (
       <Router>
         <div className="App wrapper">
