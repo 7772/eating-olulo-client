7d453a7c773a078fd239423e7f8dd21bf411a00a
landon <landon@olulo.io>
2020-02-11 08:53:04 +0900

    Implements MealTiket detail

--- src/components/MealTiket/index.tsx
+++ src/components/MealTiket/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import {Jumbotron, Button} from 'reactstrap';
+import { DateHelper } from '../../helpers';
 
 
 const MealTiket = ({mealTiket}: any) => {
+  const store = mealTiket.store;
+  const startTime = mealTiket.startTime;
+  const endTime = mealTiket.endTime;
+
+  const getRemainTime = () => {
+    const timestamp = DateHelper.getTimestampByFullDate(endTime) - DateHelper.getTimestamp();
+    const min = DateHelper.secToMin(timestamp / 1000);
+
+    return DateHelper.minToHourMinFormat(min);
+  };
+
+  const onClickJoinIn = () => {
+    alert('TODO: 참여하기');
+  };
+
   return (
     <div style={{marginTop: 5}}>
       <Jumbotron className={'container'}>
-        <h1 className="display-3">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+        <h1 className="display-3">{store.name}</h1>
+        <p className="lead">{store.phoneNumber}</p>
         <hr className="my-2" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <p className="lead">
-          <Button color="primary">Learn More</Button>
+        <p>종료까지 {getRemainTime()} 남았습니다!</p>
+        <p className="lead" style={{marginTop: 50}}>
+          <Button color="primary" onClick={onClickJoinIn}>참여하기</Button>
         </p>
       </Jumbotron>
     </div>
--- src/helpers/DateHelper/index.ts
+++ src/helpers/DateHelper/index.ts
@@ -9,6 +9,27 @@ class DateHelper {
   static getFullDateByTime(time: string, format: string | undefined = 'YYYY-MM-DDT') {
     return `${this.dayjs().format(format)}${time}`;
   }
+
+  static getTimestampByFullDate(fullDateString: string): number {
+    const timestamp = dayjs(fullDateString).unix() * 1000;
+
+    return timestamp;
+  }
+
+  static getTimestamp(): number {
+    return dayjs().unix() * 1000;
+  }
+
+  static secToMin(sec: number): number {
+    return Math.ceil(sec / 60);
+  }
+
+  static minToHourMinFormat(min: number): string {
+    let hours = Math.floor(min / 60);
+    let minutes = min % 60;
+    
+    return (hours > 0 ? hours + '시간' : '') + ' ' + (minutes > 0 || hours == 0 ? minutes + '분' : '');
+  }
 }
 
 
